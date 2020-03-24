983df9fa414b53e5e74775944df23296d963806f
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2020-03-24 21:44:39 +0100

    Started BeatSaber Prototype + cleanup

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> prefabs;
-    public Transform spawnPoint;
+    public List<Transform> spawnPoints;
+    private float timer;
+    public float beat = 1;
+
+    //public Transform spawnPoint;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -13,12 +17,28 @@ public class Spawner : MonoBehaviour
     /// </summary>
     void Start()
     {
-        if (!spawnPoint) {
-            spawnPoint = gameObject.GetComponent<Transform>();
+        if (spawnPoints.Count < 1) {
+            //spawnPoints = gameObject.GetComponent<Transform>();
+            spawnPoints.Add(gameObject.GetComponent<Transform>());
         }
     }
 
+    private void Update() {
+        if (timer > beat) {
+            SpawnRandomly(1);
+            
+            timer -= beat;
+        }
+
+        timer += Time.deltaTime;
+    }
+
     public void Spawn(int index) {
-        Instantiate(prefabs[index], spawnPoint.position, spawnPoint.rotation);
+        Instantiate(prefabs[index], spawnPoints[0].position, spawnPoints[0].rotation);
+    }
+
+    public void SpawnRandomly(int index) {
+        int rnd = Random.Range(0, spawnPoints.Count);
+        Instantiate(prefabs[index], spawnPoints[rnd].position, spawnPoints[rnd].rotation);
     }
 }
